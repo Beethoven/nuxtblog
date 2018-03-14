d8d22149d7c2480a37045802834f2618e4a9f009
Roberto <robertomedeirosgarcia@gmail.com>
2018-03-14 07:41:26 -0300

    First Layout

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,33 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        toquedigital
+        Toque Digital
       </h1>
       <h2 class="subtitle">
-        Toque Digital - Gráfica inteligente
+        Gráfica inteligente
       </h2>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
-          class="button--green">Documentation</a>
+          class="button--green" rel="noopener">Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey" rel="noopener">GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
     AppLogo
   }
-}
+};
 </script>
 
 <style>
@@ -42,7 +42,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
